5dfb135351c155ca97158ad9563bd6be91185134
jacksolaro <jacksolaro@gmail.com>
2020-11-03 19:30:25 -0800

    added delete route

--- public/index.js
+++ public/index.js
@@ -2,17 +2,58 @@ const workoutBtn = $("#workoutBtn");
 const exerciseBtn = $("#exerciseBtn");
 const workoutHistory = $("#workoutHistory");
 
+fetch("/api/workouts")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(workout => {
+        const newWorkoutEl = $("<div>")
+        newWorkoutEl.addClass("card m-2")
+        newWorkoutEl.attr("id",workout._id)
 
-workoutBtn.on("click", function(event) {
-    event.preventDefault();
-    // TODO: Create Workout in Database
+        const workoutName = $("<h3>")
+        workoutName.text(workout.name)
 
-    // TODO: For each workout in Database, create cards. Give card the ID of the workout
+        newWorkoutEl.append(workoutName)
+        workout.activities.forEach(activity => {
+            console.log(activity)
+            const newActivity = $("<div>")
+            newActivity.addClass("card m-2")
+            newActivity.attr("id",activity._id)
 
-})
+            const name = $("<h4>")
+            const type = $("<p>")
+            const weight = $("<p>")
+            const sets = $("<p>")
+            const reps = $("<p>")
+            const duration = $("<p>")
+            const distance = $("<p>")
+            const deleteBtn = $("<button>")
+            
+            name.text(activity.name)
+            type.text(activity.type)
+            weight.text(activity.weight)
+            sets.text(activity.sets)
+            reps.text(activity.reps)
+            duration.text(activity.duration)
+            distance.text(activity.distance)
+            deleteBtn.text("Delete")
 
-exerciseBtn.on("click", function(event) {
-    event.preventDefault();
-    // TODO: Create Exercise, push that id to the right workout 
+            newActivity.append(name,type,weight,sets,reps,duration,distance,deleteBtn)
+            newWorkoutEl.append(newActivity)
+            
+        })
+        workoutHistory.append(newWorkoutEl)
+    })
+    // data[0].activities.forEach(workout => {
 
-})
+    //     const newCardEl = $("<div>")
+    //     newCardEl.addClass("card")
+
+
+    //     const title = $("<h3>")
+    //     title.text(workout.name)
+
+    //     newCardEl.append(title)
+    //     workoutHistory.append(newCardEl)
+    // });
+  });
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
+const { db } = require("../models/Activity.js");
 const Activity = require("../models/Activity.js");
 const Workout = require("../models/Workout.js");
+
+router.get("/", (req, res) => {
+  Workout.find({})
+    .populate("activities")
+    .then(dbWorkout => {
+      console.log(dbWorkout)
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
 router.get("/api/activities", (req, res) => {
   Activity.find({})
@@ -14,6 +27,7 @@ router.get("/api/activities", (req, res) => {
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
+    .populate("activities")
     .then(dbWorkout => {
       res.json(dbWorkout);
     })
@@ -24,8 +38,9 @@ router.get("/api/workouts", (req, res) => {
 
 router.post("/api/activity", ({ body }, res) => {
   Activity.create(body)
+    // TODO: Push activity into the corresponding Workout
+    .then(({ _id }) => Workout.findOneAndUpdate({}, { $push: { activities: _id } }, { new: true }))
     .then(dbActivity => {
-      // TODO: Push activity into the corresponding Workout
       res.json(dbActivity);
     })
     .catch(err => {
@@ -41,6 +56,16 @@ router.post("/api/workout", ({ body }, res) => {
     .catch(err => {
       res.status(400).json(err);
     });
+});
+
+router.delete("/api/activity/", ({ body }, res) => {
+  console.log({ body })
+  Activity.deleteOne({ _id: body._id })
+  .then(
+    res.send(200)
+  ).catch(err => {
+    res.status(400).json(err)
+  })
 });
 
 module.exports = router;
